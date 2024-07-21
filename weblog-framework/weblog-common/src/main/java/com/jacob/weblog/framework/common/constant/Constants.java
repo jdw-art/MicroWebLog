c231aa79333c1e1b6ce4023315dd82656f751b13
jacob <jdw17733356203@icloud.com>
2024-07-21 20:28:52 +0800

    feat(weblog-common):Global static instance class

---
+++ weblog-framework/weblog-common/src/main/java/com/jacob/weblog/framework/common/constant/Constants.java
@@ -0,0 +1,21 @@
+package com.jacob.weblog.framework.common.constant;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @Author: Jacob
+ * @Description: 全局静态实例类
+ * @Date: 2024-07-21 20:05
+ * @Version: 1.0
+ */
+public interface Constants {
+    /**
+     * 月-日 格式
+     */
+    DateTimeFormatter MONTH_DAY_FORMATTER = DateTimeFormatter.ofPattern("MM-dd");
+
+    /**
+     * 年-月-日 小时-分钟-秒
+     */
+    DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+}
