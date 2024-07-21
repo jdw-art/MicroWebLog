34dfec59898a384b0d63efaa02f82e5f10d44545
jacob <jdw17733356203@icloud.com>
2024-07-21 21:50:32 +0800

    feat(weblog-web):weblog-web module

---
+++ weblog-web/src/main/java/com/jacob/weblog/WebLogWebApplication.java
@@ -0,0 +1,17 @@
+package com.jacob.weblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Jacob
+ * @Description: weblog-web启动类
+ * @Date: 2024-07-21 21:38
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class WebLogWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebLogWebApplication.class, args);
+    }
+}
