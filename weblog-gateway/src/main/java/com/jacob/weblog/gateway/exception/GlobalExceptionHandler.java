7efa202a08a246cc53728a39ab8dcce45b0ee66d
jacob <jdw17733356203@icloud.com>
2024-07-21 15:14:58 +0800

    feat(weblog-gateway):weblog-gateway module

---
+++ weblog-gateway/src/main/java/com/jacob/weblog/gateway/exception/GlobalExceptionHandler.java
@@ -0,0 +1,72 @@
+package com.jacob.weblog.gateway.exception;
+
+import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotPermissionException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jacob.weblog.framework.common.enums.ResponseCodeEnum;
+import com.jacob.weblog.framework.common.response.Response;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author: Jacob
+ * @Description: 网关全局异常处理类
+ * @Date: 2024-07-20 19:11
+ * @Version: 1.0
+ */
+@Component
+@Slf4j
+public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
+
+    @Resource
+    private ObjectMapper objectMapper;
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        // 获取响应对象
+        ServerHttpResponse response = exchange.getResponse();
+
+        log.error("==> 全局异常捕获：", ex);
+
+        // 响参
+        Response<?> result;
+        // 根据捕获的异常信息类型，设置不同的响应状态码和响应信息
+        if (ex instanceof NotLoginException) {
+            // 设置 401 状态码
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            // 构建响应结果
+            result = Response.fail(ResponseCodeEnum.UNAUTHORIZED.getErrorCode(), ResponseCodeEnum.UNAUTHORIZED.getErrorMessage());
+        } else if (ex instanceof NotPermissionException) {
+            // 无权限异常
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            // 构建响应结果
+            result = Response.fail(ResponseCodeEnum.UNAUTHORIZED.getErrorCode(), ResponseCodeEnum.UNAUTHORIZED.getErrorMessage());
+        } else {
+            // 其他异常，则统一提示 “系统繁忙” 异常
+            result = Response.fail(ResponseCodeEnum.SYSTEM_ERROR);
+        }
+
+        // 设置响应头的内容类型为 application/json; charset=utf-8，表示响应体为JSON格式
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON_UTF8);
+        // 设置 body 响应体
+        return response.writeWith(Mono.fromSupplier(() -> {
+            // 使用Mono.fromSupplier()方法，将响应体封装成Mono对象，并返回
+            DataBufferFactory bufferFactory = response.bufferFactory();
+            try {
+                // 使用 ObjectMapper 将result 转换成 JSON 字节数组
+                return bufferFactory.wrap(objectMapper.writeValueAsBytes(result));
+            } catch (Exception e) {
+                // 如果转换过程中出现异常，则返回空字节数组
+                return bufferFactory.wrap(new byte[0]);
+            }
+        }));
+    }
+}
