ab1f5bcc2cc0b1b1d246a525a7bfed0584f1a027
jacob <jdw17733356203@icloud.com>
2024-07-21 15:18:19 +0800

    feat(weblog-framework-context):weblog-framework-context module

---
+++ weblog-framework/weblog-spring-bot-starter-biz-context/src/main/java/com/jacob/weblog/framework/biz/context/holder/LoginUserContextHolder.java
@@ -0,0 +1,50 @@
+package com.jacob.weblog.framework.biz.context.holder;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+import com.jacob.weblog.framework.common.constant.GlobalConstants;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @Author: Jacob
+ * @Description: TODO
+ * @Date: 2024-07-20 20:41
+ * @Version: 1.0
+ */
+public class LoginUserContextHolder {
+
+    // 初始化一个 ThreadLocal 变量
+    private static final ThreadLocal<Map<String, Object>> LOGIN_USER_CONTEXT_THREAD_LOCAL
+            = TransmittableThreadLocal.withInitial(HashMap::new);
+
+    /**
+     * 设置用户 ID
+     *
+     * @param value
+     */
+    public static void setUserId(Object value) {
+        LOGIN_USER_CONTEXT_THREAD_LOCAL.get().put(GlobalConstants.USER_ID, value);
+    }
+
+    /**
+     * 获取用户 ID
+     *
+     * @return
+     */
+    public static Long getUserId() {
+        Object value = LOGIN_USER_CONTEXT_THREAD_LOCAL.get().get(GlobalConstants.USER_ID);
+        if (Objects.isNull(value)) {
+            return null;
+        }
+        return Long.valueOf(value.toString());
+    }
+
+    /**
+     * 删除 ThreadLocal
+     */
+    public static void remove() {
+        LOGIN_USER_CONTEXT_THREAD_LOCAL.remove();
+    }
+}
