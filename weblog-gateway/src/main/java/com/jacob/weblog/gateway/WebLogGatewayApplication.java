4ab87ebff3ba9e9a7b9a99cb78e91aa7d0b1413a
jacob <jdw17733356203@icloud.com>
2024-07-21 15:13:51 +0800

    feat(weblog-gateway):weblog-gateway module

---
+++ weblog-gateway/src/main/java/com/jacob/weblog/gateway/WebLogGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.jacob.weblog.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Jacob
+ * @Description: weblog网关启动类
+ * @Date: 2024-07-20 17:46
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class WebLogGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebLogGatewayApplication.class, args);
+    }
+}
---
+++ weblog-gateway/src/main/java/com/jacob/weblog/gateway/auth/SaTokenConfigure.java
@@ -0,0 +1,65 @@
+package com.jacob.weblog.gateway.auth;
+
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotPermissionException;
+import cn.dev33.satoken.exception.NotRoleException;
+import cn.dev33.satoken.reactor.filter.SaReactorFilter;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Jacob
+ * @Description: SaToken配置类
+ * @Date: 2024-07-20 18:41
+ * @Version: 1.0
+ */
+@Configuration
+@Slf4j
+public class SaTokenConfigure {
+
+    // 注册 SaToken 全局过滤器
+    @Bean
+    public SaReactorFilter getSaReactorFilter() {
+        return new SaReactorFilter()
+                // 拦截地址
+                .addInclude("/**")    /* 拦截全部path */
+                // 鉴权方法：每次访问进入
+                .setAuth(obj -> {
+                    log.info("===============> SaReactorFilter, Path: {}", SaHolder.getRequest().getRequestPath());
+                    // 登录校验
+                    SaRouter.match("/**") // 拦截所有路由
+                            .notMatch("/auth/login") // 排除登录接口
+                            .check(r -> StpUtil.checkLogin()) // 校验是否登录
+                    ;
+
+                    // 权限认证 -- 不同模块, 校验不同权限
+//                    SaRouter.match("/auth/user/logout", r -> StpUtil.checkPermission("user"));
+//                    SaRouter.match("/auth/user/logout", r -> StpUtil.checkRole("admin"));
+//                    SaRouter.match("/auth/logout", r -> StpUtil.checkPermission("app:note:publish"));
+                    // SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+                    // SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+                    // SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+
+                    // 更多匹配 ...  */
+                })
+                // 异常处理方法，每次 setAuth 函数出现异常进入
+                .setError(e -> {
+//                    return SaResult.error(e.getMessage());
+                    // 手动抛出异常，抛给全局异常处理器
+                    if (e instanceof NotLoginException) {
+                        // 未登录异常
+                        throw new NotLoginException(e.getMessage(), null, null);
+                    } else if (e instanceof NotPermissionException || e instanceof NotRoleException) {
+                        // 权限不足，或不具备角色，统一抛出权限不足异常
+                        throw new NotPermissionException(e.getMessage());
+                    } else {
+                        throw new RuntimeException(e.getMessage());
+                    }
+                })
+                ;
+    }
+}
