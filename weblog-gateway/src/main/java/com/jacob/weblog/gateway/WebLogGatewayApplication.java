e2cb50fb59cb9e84b73a71398cbbcd94aab82d4a
jacob <jdw17733356203@icloud.com>
2024-07-21 16:57:43 +0800

    fix(weblog-common):Gateway module startup class

--- weblog-gateway/src/main/java/com/jacob/weblog/gateway/WebLogGatewayApplication.java
+++ weblog-gateway/src/main/java/com/jacob/weblog/gateway/WebLogGatewayApplication.java
@@ -2,6 +2,7 @@ package com.jacob.weblog.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @Author: Jacob
@@ -9,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2024-07-20 17:46
  * @Version: 1.0
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class WebLogGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebLogGatewayApplication.class, args);
