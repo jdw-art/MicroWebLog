d290a8d3702ff13aa0dd2e65e9841555935fe567
jdw-art <jdw17733356203@icloud.com>
2024-08-16 21:43:14 +0800

    add(weblog-web):Added hot data status code

--- weblog-framework/weblog-common/src/main/java/com/jacob/weblog/framework/common/enums/ResponseCodeEnum.java
+++ weblog-framework/weblog-common/src/main/java/com/jacob/weblog/framework/common/enums/ResponseCodeEnum.java
@@ -33,6 +33,7 @@ public enum ResponseCodeEnum implements BaseExceptionInterface {
     CATEGORY_CAN_NOT_DELETE("20011", "该分类下包含文章，请先删除对应文章，才能删除！"),
     TAG_CAN_NOT_DELETE("20012", "该标签下包含文章，请先删除对应文章，才能删除！"),
     WIKI_NOT_FOUND("20013", "该知识库不存在"),
+    HOT_ARTICLE_NOT_FOUND("20014", "当前没有访问数据"),
     ;
 
     // 异常码
